3477cc2a1a406de6ec89d7f187422fd715f4bfd6
schmidtp0740 <schmidtp0740@gmail.com>
2018-11-01 19:18:09 -0500

    updated get all rx ledger function

--- src/app/services/API/blockAPI.js
+++ src/app/services/API/blockAPI.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
-const url = "http://"+window.location.hostname+":8080/getData";
+// Dev url
+const url = "http://129.213.66.90:8080/rxledger";
+
+// Prod url
+// const url = "http://"+window.location.hostname+":8080/getData";
 export async function getAllData(){
   console.log(url);  
   return axios.get(url)
       .then(function(response) {
-          var data = JSON.parse(JSON.parse(response.data).result);
+          var data = response.data
+
           return data;
       })
       .catch(function(error) {
