eaa9e0f84b87d61240b00dad4c04eeeda573d474
schmidtp0740 <schmidtp0740@gmail.com>
2018-11-05 11:49:00 -0600

    changed title to Prescription ledger

--- src/app/views/home/Home.js
+++ src/app/views/home/Home.js
@@ -71,7 +71,7 @@ class Home extends PureComponent {
         <Row>
             <Col span={12} offset={6}>
           <h1 className="headerTitle">
-            Rx Ledger
+            Prescription Ledger
           </h1>
           </Col>  
           </Row>
